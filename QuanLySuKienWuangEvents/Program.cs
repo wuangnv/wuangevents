7d068a604dade84702ca140d046b898d675a1f01
VinhQuang <dh52201310@gmail.com>
2026-05-28 09:15:00 +0700

    Khoi tao du an QuanLySuKienWuangEvents va cau truc ASP.NET Core MVC

---
+++ QuanLySuKienWuangEvents/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using QuanLySuKienWuangEvents.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Khởi tạo SQL connection helper đóng gói Dapper
+Db.Init(builder.Configuration.GetConnectionString("DefaultConnection") ?? "");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<QuanLySuKienWuangEvents.Services.EmailService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/DangNhap";
+        options.LogoutPath = "/Account/DangXuat";
+        options.AccessDeniedPath = "/Account/DangNhap";
+        options.Cookie.Name = "WuangEvents.Student";
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
